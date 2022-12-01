7a98125fa7d0ea6f17ed078415749b8c4b2fb6fd
goporo <goporolo1@gmail.com>
2022-12-01 17:26:50 +0700

    update sidebar

--- client-customer/src/components/Layout/SidebarRow.js
+++ client-customer/src/components/Layout/SidebarRow.js
@@ -1,21 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
 
 
-const SidebarRow = ({ display, to, Icon, badge, isActive }) => {
+const SidebarRow = ({ display, to, Icon, badge, isActivea }) => {
     return (
         <NavLink
             to={to}
-            className="flex space-x-2 text-gray-600 text-md items-center py-3 relative"
+            className={({ isActive }) => isActive ? " text-orange-500 font-semibold" : "text-gray-600"}
         >
-            <span className="text-2xl">
-                <Icon />
-            </span>
-            <span>{display}</span>
-            {badge && (
-                <span className="absolute right-0 bg-[#f25e35] text-white h-6 w-6 flex justify-center items-center p-1 text-sm rounded-full">
-                    {badge}
+            <div className="flex space-x-2 text-md items-center py-3 relative hover:text-orange-400">
+                <span className="text-2xl">
+                    <Icon />
                 </span>
-            )}
+                <span>{display}</span>
+                {badge && (
+                    <span className="absolute right-0 bg-[#f25e35] text-white h-6 w-6 flex justify-center items-center p-1 text-sm rounded-full">
+                        {badge}
+                    </span>
+                )}
+            </div>
 
         </NavLink>
     );
