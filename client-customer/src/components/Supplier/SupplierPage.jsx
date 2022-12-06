c75be5bfb529b5e5966d34b9dc4966e01c921207
goporo <goporolo1@gmail.com>
2022-12-07 00:30:12 +0700

    fix bug

--- client-customer/src/components/Supplier/SupplierPage.jsx
+++ client-customer/src/components/Supplier/SupplierPage.jsx
@@ -112,16 +112,12 @@ const foods = [
 
 export default function SupplierPage({ }) {
     const dispatch = useDispatch();
-    const addToCart = () => {
+    const addToCart = (item) => {
+        console.log(item);
         dispatch(
-            cartActions.addItem({
-                // id,
-                // title,
-                // price,
-                // rating,
-                // image,
-                // supplier,
-            })
+            cartActions.addItem(
+                item
+            )
         );
     };
 
@@ -130,7 +126,7 @@ export default function SupplierPage({ }) {
             <div className="flex flex-row w-11/12 mx-auto space-x-5 my-10 bg-white p-5 rounded-lg shadow-sm">
                 <img className="w-40 mr-8" src={supplier.logo} alt="" />
                 <div className="flex flex-col ">
-                    <h1 class="font-semibold text-xl mb-3">{supplier.name}</h1>
+                    <h1 className="font-semibold text-xl mb-3">{supplier.name}</h1>
                     <p className="text-gray-500 text-sm mb-3">{supplier.location}</p>
                     <Rating className='mb-3' name="read-only" value={supplier.rating} readOnly precision={.5} />
                     <div className="flex-row flex items-center mb-3">
@@ -156,17 +152,17 @@ export default function SupplierPage({ }) {
                     }
                 </div>
                 <div className="w-8/12 bg-white p-5 rounded-lg shadow-sm">
-                    <form class="flex items-center mb-3">
-                        <label class="sr-only">Search</label>
-                        <div class="relative w-full">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                    <form className="flex items-center mb-3">
+                        <label className="sr-only">Search</label>
+                        <div className="relative w-full">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                             </div>
-                            <input type="text" id="simple-search" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                            <input type="text" id="simple-search" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
                         </div>
-                        <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            <span class="sr-only">Search</span>
+                        <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            <span className="sr-only">Search</span>
                         </button>
                     </form>
                     <div>
@@ -175,13 +171,13 @@ export default function SupplierPage({ }) {
                             <div key={index} className='flex flex-row justify-between mb-3 items-center'>
                                 <img className='w-[64px] h-[64px] rounded-lg' src={item.image} alt="" />
                                 <div className='w-8/12'>
-                                    <h1 class="font-semibold text-xl mb-3">{item.title.toUpperCase()}</h1>
+                                    <h1 className="font-semibold text-xl mb-3">{item.title.toUpperCase()}</h1>
                                     <p className="text-gray-500 text-sm mb-3">Lorem ipsum dolor sit amet, bo corrupti doloribus eum delectus voluptatibus magni rerum, voluptas recusandae illo.</p>
                                 </div>
                                 <div className='flex flex-row items-center'>
                                     <p className="text-blue-500 text-sm mr-3">${item.price}</p>
                                     <button
-                                        onClick={addToCart}
+                                        onClick={() => addToCart(item)}
                                         className='cursor-pointer hover:opacity-80 rounded-lg bg-red-600 text-white text-md font-semibold py-1 px-3'>+</button>
                                 </div>
                             </div>
