a94cd7af34d022e8ce8cc4a3b2a124b829e60f4a
goporo <goporolo1@gmail.com>
2022-12-10 19:38:10 +0700

    order page

--- client-branch/src/index.js
+++ client-branch/src/index.js
@@ -2,7 +2,6 @@ import GlobalStyles from 'components/GlobalStyles/GlobalStyles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css";
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'tippy.js/dist/tippy.css';
