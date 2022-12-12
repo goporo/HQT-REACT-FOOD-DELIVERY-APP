c2348ac7817be20625005f83ab213da64722e9b0
Khanhvy12 <khanhvy020612@gmail.com>
2022-12-12 22:32:54 +0700

    done register page shipper

---
+++ client-shipper/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
