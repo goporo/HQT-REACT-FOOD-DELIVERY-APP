6c1405e2ded6d732a8f9419aa1783ce5b4b544bd
Khanhvy12 <khanhvy020612@gmail.com>
2022-12-13 21:21:22 +0700

    login & update register

--- client-shipper/src/App.js
+++ client-shipper/src/App.js
@@ -1,4 +1,4 @@
-import Layout from "./pages/Register";
+import Layout from "./pages/Login";
 
 
 
--- client-shipper/src/routes/Routers.js
+++ client-shipper/src/routes/Routers.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import Login from "../pages/Login";
 import Register from "../pages/Register";
 const Routers = () => {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   );
