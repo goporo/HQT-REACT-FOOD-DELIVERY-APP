099584940d1058e92f67ff3d2f8b1f007e5c50f3
Phạm Minh Tài <pmtai20@clc.fitus.edu.vn>
2022-12-18 02:21:52 +0700

    Update supplierController.js

--- server_hqt/api/supplier/supplierController.js
+++ server_hqt/api/supplier/supplierController.js
@@ -2,6 +2,27 @@ const sql = require("mssql")
 const config = require('../../dbConfig')
 
 exports.getSuppliers = async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", " GET");
+    res.header("Access-Control-Allow-Headers", "Content-Type")
+    try {
+     
+        let pool = await sql.connect(config)
+        let result = await pool.request().query('SELECT * FROM DOITAC')
+
+        sql.close()
+        var success=true
+        var message="Success Get"
+        var data=result.recordset
+        res.json({success,message,data})
+    }
+    catch (error){
+        var success=false
+        var message=error.message
+        var data={}
+        res.json({success,message,data})
+    }
+
 
 };
 
