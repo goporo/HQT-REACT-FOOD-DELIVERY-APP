ad779e446d0aeb94596a8ad4c722902dfd3252e3
goporo <goporolo1@gmail.com>
2022-12-18 20:46:42 +0700

    fix bug

---
+++ server_hqt/api/supplier/supplierRouter.js
@@ -0,0 +1,13 @@
+
+
+const express = require('express');
+const router = express.Router();
+const controller = require("./supplierController");
+
+router.get('/', controller.getSuppliers);
+router.get('/:id', controller.getSuppliersDetails);
+
+
+
+
+module.exports = router;
