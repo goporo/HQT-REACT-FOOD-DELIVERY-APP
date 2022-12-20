e176f16731cc304be18fdd34683f4131dd6512d9
goporo <goporolo1@gmail.com>
2022-12-20 22:31:49 +0700

    get orders

--- client-customer/src/components/Order/Order.jsx
+++ client-customer/src/components/Order/Order.jsx
@@ -2,95 +2,32 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import LazyLoad from "react-lazy-load";
-
-const orders = [
-  {
-    orderID: "O001",
-    items: [
-      {
-        id: "food_1",
-        title: "chicken deluxe fry",
-        price: 150.0,
-        rating: 4.3,
-        image: "/images/chicken_deluxe_fry.jpg",
-        supplier: "the-alley",
-
-      },
-      {
-        id: "food_2",
-        title: "Pasta",
-        price: 70.0,
-        rating: 4.3,
-        image: "/images/pasta.jpg",
-        supplier: "the-alley",
-        status: "Delivered",
-      }
-    ],
-    shipper: "Nguyen Hoai An",
-    status: "Validating",
-  },
-  {
-    orderID: "O002",
-    items: [
-      {
-        id: "food_1",
-        title: "chicken deluxe fry",
-        price: 150.0,
-        rating: 4.3,
-        image: "/images/chicken_deluxe_fry.jpg",
-        supplier: "the-alley",
-
-      },
-      {
-        id: "food_2",
-        title: "Pasta",
-        price: 70.0,
-        rating: 4.3,
-        image: "/images/pasta.jpg",
-        supplier: "the-alley",
-        status: "Delivered",
-      }
-    ],
-    shipper: "Nguyen Hoai An",
-    status: "Delivered",
-  },
-]
 
 
+
+const makh = '1';
 
 const Order = () => {
   const [orders, setOrders] = useState([]);
 
+
+
   useEffect(() => {
-    const getData = () => {
+    const getData = async () => {
 
       axios.put(`/order/customer`, {
-
-        "MAKH": "1",
-
+        "MAKH": makh,
       })
         .then(function (res) {
-          let temp = res.data.data.orders;
-          let temp2 = res.data.data.orders_details;
+          let temp = res.data.data;
 
-          temp2 = temp2.map(item => {
-            return {
-              id: item.MAMONAN,
-              title: item.TENMONAN,
-              price: item.GIA
-            }
-          });
           temp = temp.map((item, index) => {
             return {
               orderID: item.MADH,
-              items: temp2[index],
               shipper: item.TEN,
               status: item.TRANGTHAIDH
             }
           });
-          console.log(temp2);
-
           setOrders(temp);
         })
         .catch(function (e) {
@@ -111,22 +48,17 @@ const Order = () => {
             ) : (
               <div className="">
                 {orders.map((item, index) => (
-                  <div key={index} className="mb-10 bg-white p-5 shadow-sm rounded-lg">
+                  <div className="mb-10 bg-white p-5 shadow-sm rounded-lg">
                     <div className="flex items-center justify-between mb-2">
                       <div className="font-semibold bg-orange-400 text-white text-sm w-fit rounded-md py-2 px-3">ORDER: {item.orderID}</div>
                       <div className={`flex flex-row justify-center text-white text-sm py-1 px-2 rounded-md ${item.status === "Delivered" ? "bg-green-500" : "bg-orange-400"}`}>
                         {item.status}
                       </div>
                     </div>
-                    {orders.items.map((item, index) => (
-                      orders.items.length ?
-                        <div key={index} className="flex flex-row space-y-2">
-                          <div className="w-3/12 text-lg capitalize">{item.id}</div>
-                          <div className="w-4/12 text-lg capitalize">{item.title}</div>
-                          <p className="w-2/12 text-orange-500 text-lg">{item.price}đ</p>
-                        </div >
-                        : null
-                    ))}
+                    <Tr key={index} item={item} madh={item.orderID} />
+
+
+
                     <hr className="my-2" />
                     <div className="flex items-end flex-col space-y-2">
                       <div className="text-lg">Người giao hàng: <span className="text-orange-400">{item.shipper}</span></div>
@@ -144,7 +76,6 @@ const Order = () => {
                       }
                     </div>
                   </div>
-
                 ))}
               </div>
             )}
@@ -154,6 +85,48 @@ const Order = () => {
     </section>
   );
 };
+
+
+const Tr = (props) => {
+  const [orderDetails, setOrderDetails] = useState([]);
+
+  useEffect(() => {
+    const getDetails = (makh, madh) => {
+      return axios.put(`/order/orderdetails`, {
+        "MAKH": makh,
+        "MADH": madh,
+      })
+        .then(function (res) {
+          let temp = res.data.data;
+          temp = temp.map(item => {
+            return {
+              id: item.MAMONAN,
+              title: item.TENMONAN,
+              price: item.GIA
+            }
+          });
+          console.log(temp);
+          setOrderDetails(temp);
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
+    };
+    getDetails(makh, props.madh);
+  }, [])
+
+  return (
+    orderDetails.length ?
+      orderDetails.map((item, index) => (
+        <div key={index} className="flex flex-row space-y-2">
+          <div className="w-3/12 text-lg capitalize">{item.id}</div>
+          <div className="w-4/12 text-lg capitalize">{item.title}</div>
+          <p className="w-2/12 text-orange-500 text-lg">{item.price}đ</p>
+        </div >
+      )) : null
+  )
+}
+
 
 
 export default Order;
