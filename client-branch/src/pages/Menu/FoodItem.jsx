cc367e5e1de880dd4f3c00ee568efe1f27b58495
Thanh Hằng <ntthanhhang2k2@gmail.com>
2022-12-21 00:23:00 +0700

    [Branch] implement update, delete, add, view detail food

--- client-branch/src/pages/Menu/FoodItem.jsx
+++ client-branch/src/pages/Menu/FoodItem.jsx
@@ -6,30 +6,31 @@ import { useState } from "react";
 import { formatVND } from "../../utils/currencyFormatter";
 
 const FoodItem = ({ f }) => {
-    const [loading, setLoading] = useState(true)
-    return (
-        <div className="h-120">
-            <div className="cursor-pointer relative h-80 overflow-hidden rounded-xl">
-                {/* {loading && <Skeleton height={192} />} */}
-                <Link to={`/menu/${f.id}`}>
-                    <LazyLoad className="h-full w-full">
-                        <img src={f.image} alt={f.title}
-                            className="h-full w-full object-cover hover:scale-110 transition duration-200"
-                        />
-                    </LazyLoad>
-                </Link>
-            </div>
-            <div className="flex justify-between items-center px-2 mt-4 capitalize">
-                <h1 className="cursor-pointer font-semibold text-md">
-                    <Link to={`/menu/${f.id}`}>{f.title}</Link>
-                </h1>
+  const [food, setFood] = useState(f);
 
-                <div className="bg-[#ffede6] text-[#ff4f00] text-2xs font-semibold px-4 py-1 rounded-md whitespace-nowrap">
-                    {formatVND(f.price)}
-                </div>
-            </div>
+  return (
+    <div className="h-120">
+      <div className="cursor-pointer relative h-80 overflow-hidden rounded-xl">
+        {/* {loading && <Skeleton height={192} />} */}
+        <Link to={`/menu/${food.MAMONAN}`}>
+          <LazyLoad className="h-full w-full">
+            <img src={`https://statics.vinpearl.com/com-tam-ngon-o-sai-gon-0_1630563211.jpg`} alt={food.TENMONAN}
+              className="h-full w-full object-cover hover:scale-110 transition duration-200"
+            />
+          </LazyLoad>
+        </Link>
+      </div>
+      <div className="flex justify-between items-center px-2 mt-4 capitalize">
+        <h1 className="cursor-pointer font-semibold text-md">
+          <Link to={`/menu/${food.MAMONAN}`}>{food.TENMONAN}</Link>
+        </h1>
+
+        <div className="bg-[#ffede6] text-[#ff4f00] text-2xs font-semibold px-4 py-1 rounded-md whitespace-nowrap">
+          {formatVND(food.GIA)}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodItem;
--- client-branch/src/pages/Menu/MenuPage.jsx
+++ client-branch/src/pages/Menu/MenuPage.jsx
@@ -1,15 +1,16 @@
-import foods from "../../assets/fake-data/foods";
 import FoodItem from "./FoodItem";
 import { useEffect, useState } from "react";
+import Modal from 'react-modal';
 
 import LazyLoad from 'react-lazy-load';
 import 'react-loading-skeleton/dist/skeleton.css'
 import { formatVND } from "../../utils/currencyFormatter";
 import { Link } from "react-router-dom";
-
+import AddFoodModal from "./AddFoodModal";
 
 const MenuPage = () => {
-  const [abcs, setAbcs] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const addFood = async () => {
     const response = await fetch("http://localhost:5000/supplier/food/1", {
@@ -39,9 +40,7 @@ const MenuPage = () => {
   async function getFoods() {
     const response = await fetch("http://localhost:5000/supplier/1");
     const data = await response.json();
-    setAbcs(data.data.foods);
-
-    console.log(data);
+    setFoods(data.data.foods);
   }
 
   useEffect(() => {
@@ -54,49 +53,21 @@ const MenuPage = () => {
         <div className="flex justify-between items-center w-11/12 mt-10 mb-8 mx-auto">
           <h2 className="text-3xl font-semibold">Today Menu</h2>
 
-          <button className="bg-yellow-500 text-white px-10 py-3 rounded-xl font-semibold" onClick={addFood}>
+          <button className="bg-yellow-500 text-white px-10 py-3 rounded-xl font-semibold" onClick={() => setIsModalOpen(true)}>
             Add
           </button>
         </div>
 
         <div className="w-11/12 mx-auto">
           <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-5 gap-y-16">
-            {/* {foods.map((food, index) => (
+            {foods.map((food, index) => (
               <FoodItem key={index} f={food} />
-            ))} */}
-
-            {
-              abcs.map((f, index) => {
-                return (
-                  <>
-                          <div className="h-120">
-            <div className="cursor-pointer relative h-80 overflow-hidden rounded-xl">
-                {/* {loading && <Skeleton height={192} />} */}
-                <Link to={`/menu/${f.MAMONAN}`}>
-                    <LazyLoad className="h-full w-full">
-                            <img src={`https://statics.vinpearl.com/com-tam-ngon-o-sai-gon-0_1630563211.jpg`} alt={f.TENMONAN}
-                            className="h-full w-full object-cover hover:scale-110 transition duration-200"
-                        />
-                    </LazyLoad>
-                </Link>
-            </div>
-            <div className="flex justify-between items-center px-2 mt-4 capitalize">
-                <h1 className="cursor-pointer font-semibold text-md">
-                    <Link to={`/menu/${f.MAMONAN}`}>{f.TENMONAN}</Link>
-                </h1>
-
-                <div className="bg-[#ffede6] text-[#ff4f00] text-2xs font-semibold px-4 py-1 rounded-md whitespace-nowrap">
-                    {formatVND(f.GIA)}
-                </div>
-            </div>
-        </div>
-                  </>
-                )
-              })
-            }
+            ))}
           </div>
         </div>
       </div>
+
+      <AddFoodModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
     </div>
   );
 };
