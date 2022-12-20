a53058a1bc96e81a4ad35b7ccaedb122724dfd2d
Phạm Minh Tài <pmtai20@clc.fitus.edu.vn>
2022-12-20 17:20:30 +0700

    cập nhật thực đơn

--- server_hqt/api/food/foodRouter.js
+++ server_hqt/api/food/foodRouter.js
@@ -8,6 +8,5 @@ router.get('/price', controller.price);
 router.get('/supplier', controller.supplier);
 router.get('/type', controller.type);
 
-
-
+router.patch('/supplier', controller.supplier);
 module.exports = router;
--- server_hqt/api/supplier/supplierRouter.js
+++ server_hqt/api/supplier/supplierRouter.js
@@ -7,6 +7,7 @@ const controller = require("./supplierController");
 router.get('/', controller.getSuppliers);
 router.get('/:id', controller.getSuppliersDetails);
 router.post('/food/:id',controller.addFood)
+router.patch('/food',controller.updateFood)
 
 
 
