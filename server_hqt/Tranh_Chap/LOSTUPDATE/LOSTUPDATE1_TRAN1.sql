bbf7cd296249cdd777460f00f410a2dd55afa5bd
Khanhvy12 <khanhvy020612@gmail.com>
2022-12-22 01:23:24 +0700

    update lostupdate1

---
+++ server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE1_TRAN1.sql
@@ -0,0 +1,3 @@
+﻿
+--T1: tài xế A cập nhật tình trạng đơn hàng
+exec sp_CapNhat_DH_TX_Tranhchap1 'DELIVERING', '1', '1'
---
+++ server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE1_TRAN2.sql
@@ -0,0 +1,3 @@
+﻿
+--T1: tài xế B cập nhật tình trạng đơn hàng (cùng 1 đơn với tài xế A)
+exec sp_CapNhat_DH_TX_Tranhchap1 'DELIVERING', '2', '1'
