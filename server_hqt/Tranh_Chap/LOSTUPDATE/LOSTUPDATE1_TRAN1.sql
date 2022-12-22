6bbe8884999b7a0e4845526d7acc7e69299ed65c
Khanhvy12 <khanhvy020612@gmail.com>
2022-12-22 17:22:08 +0700

    update lõi

--- server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE1_TRAN1.sql
+++ server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE1_TRAN1.sql
@@ -1,3 +1,10 @@
 ﻿
 --T1: tài xế A cập nhật tình trạng đơn hàng
-exec sp_CapNhat_DH_TX_Tranhchap1 'DELIVERING', '1', '1'
+exec sp_CapNhat_DH_TX 'DELIVERING', '1', '1'
+
+
+
+
+
+
+
--- server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE2_TRAN2.sql
+++ server_hqt/Tranh_Chap/LOSTUPDATE/LOSTUPDATE2_TRAN2.sql
@@ -3,5 +3,5 @@
 USE QL_BANHANG_TRANHCHAP
 declare @listmonan list_MONAN
 insert @listmonan values(1, '5', 1)
-exec sp_Tao_DH_TranhChap2 'VI', null, '1', '1', 500, @listmonan
+exec sp_Tao_DH_TranhChap1 'VI', null, '1', '1', 500, @listmonan
 
