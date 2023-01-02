d42cab2446045d57d2a3ea34be484e290e434c4a
Thanh Hằng <ntthanhhang2k2@gmail.com>
2023-01-02 23:42:15 +0700

    [Customer] add customer balance

--- client-customer/src/components/Layout/Header.jsx
+++ client-customer/src/components/Layout/Header.jsx
@@ -2,18 +2,33 @@ import { BiBell, BiShoppingBag } from "react-icons/bi";
 import { BsFilter } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Header = () => {
+    const [balance, setBalance] = useState(0);
+
+    const formatCurrency = (num) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + 'đ'
+    }
+
     const navigate = useNavigate();
     const onSearchHandler = (e) => {
         e.preventDefault();
         navigate(`/foods`);
         ///`/foodskeyword=${e.target[0].value}`
         e.target[0].value = "";
-
     };
+
+    useEffect(() => {
+        axios.put("/user/profile/5", {
+            "MAND": localStorage.getItem("MAKH"),
+        }).then(res => {
+            if (res?.data?.data.length > 0) {
+                setBalance(res.data.data[0].SODUVI);
+            }
+        });
+    }, []);
 
     return (
         <header className="w-11/12 mx-auto flex flex-col-reverse md:justify-between items-center md:flex-row gap-6">
@@ -46,14 +61,12 @@ const Header = () => {
                     {
                         localStorage.getItem("MAKH") ?
                             <>
-                                <Link
-                                    onClick={() => {
-                                        localStorage.removeItem("MAKH")
-                                    }}
-                                    to="/login" type="button" className="bg-red-500 text-white px-5 py-2 rounded-md">
-                                    LogOut
-                                </Link>
-                                <div className="hidden md:flex items-center space-x-3">
+                                <div className="w-full flex items-end flex-col">
+                                    <p className="text-md font-semibold">MAKH:{localStorage.getItem("MAKH")}</p>
+                                    <p className="text-xs">Balance: {formatCurrency(balance)}</p>
+                                </div>
+
+                                <div className="hidden md:flex items-center space-x-3 pr-1">
                                     <button className="rounded-full overflow-hidden h-10 xl:h-12  w-10 xl:w-12 shadow-md">
                                         <img
                                             src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
@@ -61,9 +74,15 @@ const Header = () => {
                                             alt="ava"
                                         />
                                     </button>
-                                    <p className="text-md font-semibold">MAKH:{localStorage.getItem("MAKH")}</p>
-
                                 </div>
+
+                                <Link
+                                    onClick={() => {
+                                        localStorage.removeItem("MAKH")
+                                    }}
+                                    to="/login" type="button" className="bg-red-500 text-white px-5 py-2 rounded-md">
+                                    LogOut
+                                </Link>
                             </> : <Link to="/login" type="button" className="bg-red-500 text-white px-5 py-2 rounded-md">
                                 LogIn
                             </Link>
